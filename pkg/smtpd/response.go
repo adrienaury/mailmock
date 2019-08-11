5ee85ed3ddf16de9e97dee751da84cd133daffc8
adrienaury <adrienaury@gmail.com>
2019-08-11 15:42:31 +0200

    reverted backward incompatible changes

--- pkg/smtpd/response.go
+++ pkg/smtpd/response.go
@@ -37,12 +37,23 @@ package smtpd
 
 import (
 	"fmt"
+	"strings"
 )
 
 // Response holds a 3 digit code and a messsage
 type Response struct {
 	Code int16
 	Msg  string
+}
+
+// IsError returns true if the response is an error
+func (e Response) IsError() bool {
+	return strings.HasPrefix(e.String(), "5")
+}
+
+// IsSuccess returns true if the response is an success
+func (e Response) IsSuccess() bool {
+	return !e.IsError()
 }
 
 func (e Response) String() string {
